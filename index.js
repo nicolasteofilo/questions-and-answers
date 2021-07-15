b8d7a8826fa0d4257a22ab929c2d290d2e298f0d
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-07-15 16:04:06 -0300

    bootstrap installed

--- index.js
+++ index.js
@@ -6,24 +6,8 @@ app.set("view engine", "ejs"); // usar o ejs como view engine
 app.use(express.static('public'))
 
 // ROUTES
-app.get("/:nome?/:lang?", (req, res) => {
-  var nome = req.params.nome;
-  var lang = req.params.lang;
-  var exibirMsg = false;
-
-  var produtos = [
-    { nome: "Doritos", preco: 3.14 },
-    { nome: "Coca-Cola", preco: 5 },
-    { nome: "Leite", preco: 1.45 },
-    { nome: "Carne", preco: 5 },
-  ];
-  res.render("index", {
-    nome: nome,
-    lang: lang,
-    empresa: "Nicolas Bussines",
-    msg: exibirMsg,
-    produtos: produtos,
-  });
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 // RUNING APPLICATION

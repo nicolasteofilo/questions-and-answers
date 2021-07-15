dc3acfccc559ed0fae87591dda75ea7d5d391bc2
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-07-15 09:50:03 -0300

    update

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const express = require("express")
 const app = express()
+
+
+// EJS 
+app.set('view engine', 'ejs') // usar o ejs como view engine
+
+
+
+// ROUTES
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+
+// RUNING APPLICATION
+
+PORT = 5000
+app.listen(PORT, () => { console.log(`Servidor rodando: http://localhost:${PORT}`); })

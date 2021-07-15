343ef77dbbbdf52826060fa03b136066b65d5e20
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-07-15 15:34:23 -0300

    iniciando o projeto

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-
-// EJS 
-app.set('view engine', 'ejs') // usar o ejs como view engine
-
-
+// EJS
+app.set("view engine", "ejs"); // usar o ejs como view engine
+app.use(express.static('public'))
 
 // ROUTES
-app.get("/", (req, res) => {
-    res.render("index")
-})
+app.get("/:nome?/:lang?", (req, res) => {
+  var nome = req.params.nome;
+  var lang = req.params.lang;
+  var exibirMsg = false;
 
+  var produtos = [
+    { nome: "Doritos", preco: 3.14 },
+    { nome: "Coca-Cola", preco: 5 },
+    { nome: "Leite", preco: 1.45 },
+    { nome: "Carne", preco: 5 },
+  ];
+  res.render("index", {
+    nome: nome,
+    lang: lang,
+    empresa: "Nicolas Bussines",
+    msg: exibirMsg,
+    produtos: produtos,
+  });
+});
 
 // RUNING APPLICATION
 
-PORT = 5000
-app.listen(PORT, () => { console.log(`Servidor rodando: http://localhost:${PORT}`); })
+PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando: http://localhost:${PORT}`);
+});
